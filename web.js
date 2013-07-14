04cf841d4932112f82fea7fab87c15c71b08948a
Ubuntu <ubuntu@ip-172-31-8-227.us-west-2.compute.internal>
2013-07-14 06:48:02 +0000

    added web.js

--- web.js
+++ web.js
@@ -7,8 +7,7 @@ var app = express.createServer(express.logger());
 
 //});
 
-fs.readFileSync('index.html');
-buf.toString();
+fs.readFileSync("index.html","utf-8");
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
