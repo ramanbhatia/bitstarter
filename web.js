ce2aaf3195f92c04819cc1f14e81f51052cc2d57
Ubuntu <ubuntu@ip-172-31-8-227.us-west-2.compute.internal>
2013-07-14 07:11:28 +0000

    added web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ response.send(data.string('utf-8'));
  //response.send('Hello World2!');
 });
 
-response.send(fs.readFileSync('/index.html', 'utf8'));
+//response.send(fs.readFileSync('/index.html', 'utf8'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
