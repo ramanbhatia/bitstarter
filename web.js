82814587f450ed24074153081e3509cacc8621cd
Ubuntu <ubuntu@ip-172-31-8-227.us-west-2.compute.internal>
2013-07-14 07:15:43 +0000

    added web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs'); 
 var data = fs.readFileSync('index.html');
-response.send(data.tostring('utf-8'));
+response.send(data.toString('utf-8'));
  //response.send('Hello World2!');
 });
 
