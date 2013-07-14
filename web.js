e262b2922e85e79e825618632cf706ecfa0e138d
Ubuntu <ubuntu@ip-172-31-8-227.us-west-2.compute.internal>
2013-07-14 06:07:59 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 //app.get('/', function(request, response) {
   //response.send('Hello World2!');
 
-fs.readFile('/index.html', function (err, data) {
+fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
